6f78b632fae58aed4aebafc335e92dbeb2e97b3b
Santiago Gonzalez Noreña <santiago.gonzalez@s4learning.com>
2025-04-03 13:42:28 -0500

    Removing unusued imports

--- src/main/java/com/PruebaLogin/Login/JWT/JwtAuthenticationFilter.java
+++ src/main/java/com/PruebaLogin/Login/JWT/JwtAuthenticationFilter.java
@@ -2,18 +2,12 @@ package com.PruebaLogin.Login.JWT;
 
 import java.io.IOException;
 
-import com.PruebaLogin.Login.User.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-
-
-import ch.qos.logback.core.util.StringUtil;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
